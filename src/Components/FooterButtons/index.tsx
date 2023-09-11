5cd7ffd8673d12098ae2c79436a21f089c547dec
´Patrick Almeida <patrick.almeida06@gmail.com>
2023-09-10 21:56:54 -0300

    Teste com deploy

--- src/Components/FooterButtons/index.tsx
+++ src/Components/FooterButtons/index.tsx
@@ -5,33 +5,38 @@ import { useContext } from "react";
 import { AlpacaContext } from "../../AlpacaContext/Context";
 import { Dicionario } from "../../AlpacaContext/Dictionary";
 export function FooterButtons() {
-const { changeStyle} = useContext(AlpacaContext);
+  const { changeStyle } = useContext(AlpacaContext);
   const handleDownload = () => {
     const box = document.querySelector(".alpaca");
-
-    html2canvas(box).then((canvas) => {
-      const link = document.createElement("a");
-      link.href = canvas.toDataURL("image/png");
-      link.download = "alpaca.png";
-      link.click();
-    });
+    if (box instanceof HTMLCanvasElement) {
+      html2canvas(box).then((canvas) => {
+        const link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "alpaca.png";
+        link.click();
+      });
+    }
+    else{
+        console.error("Element with class '.alpaca' not found");
+    }
   };
 
   const GetRandomItem = () => {
-      const keysHair = Object.keys(Dicionario.Hair);
-      const RandomKeyHair = keysHair[Math.floor(Math.random() * keysHair.length)];
-      changeStyle(RandomKeyHair, "Hair");
+    const keysHair = Object.keys(Dicionario.Hair);
+    const RandomKeyHair = keysHair[Math.floor(Math.random() * keysHair.length)];
+    changeStyle(RandomKeyHair, "Hair");
 
-      const keysEars = Object.keys(Dicionario.Ears);
-      const RandomKeyEars = keysEars[Math.floor(Math.random() * keysEars.length)];
-      changeStyle(RandomKeyEars, "Ears");
+    const keysEars = Object.keys(Dicionario.Ears);
+    const RandomKeyEars = keysEars[Math.floor(Math.random() * keysEars.length)];
+    changeStyle(RandomKeyEars, "Ears");
 
     const keysEyes = Object.keys(Dicionario.Eyes);
     const RandomKeyEyes = keysEyes[Math.floor(Math.random() * keysEyes.length)];
     changeStyle(RandomKeyEyes, "Eyes");
 
     const keysMouth = Object.keys(Dicionario.Mouth);
-    const RandomKeyMouth = keysMouth[Math.floor(Math.random() * keysMouth.length)];
+    const RandomKeyMouth =
+      keysMouth[Math.floor(Math.random() * keysMouth.length)];
     changeStyle(RandomKeyMouth, "Mouth");
 
     const keysNeck = Object.keys(Dicionario.Neck);
@@ -43,13 +48,15 @@ const { changeStyle} = useContext(AlpacaContext);
     changeStyle(RandomKeyLeg, "Leg");
 
     const keysAcessories = Object.keys(Dicionario.Acessories);
-    const RandomKeyAcessories = keysAcessories[Math.floor(Math.random() * keysAcessories.length)];
+    const RandomKeyAcessories =
+      keysAcessories[Math.floor(Math.random() * keysAcessories.length)];
     changeStyle(RandomKeyAcessories, "Acessories");
 
     const keysBackground = Object.keys(Dicionario.Background);
-    const RandomKeyBackground = keysBackground[Math.floor(Math.random() * keysBackground.length)];
+    const RandomKeyBackground =
+      keysBackground[Math.floor(Math.random() * keysBackground.length)];
     changeStyle(RandomKeyBackground, "Background");
-  }
+  };
 
   return (
     <>
